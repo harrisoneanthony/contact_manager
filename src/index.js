fc24b02ec04e42e959443d506b6559a30e7b2f21
Harrison Anthony <harrisoneanthony@gmail.com>
2023-04-06 11:15:14 -0400

    updated to use redux framework

--- src/index.js
+++ src/index.js
@@ -1,57 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import PeopleList from './components/PeopleList';
+import AddPersonForm from './components/AddPersonForm';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './style.css';
 
-function AddPersonForm(props) {
-  const [ person, setPerson ] = useState('');
-    
-  function handleChange(e) {
-    setPerson(e.target.value);
+const initialState = {
+  contacts: ["James Smith", "Thomas Anderson", "Bruce Wayne"] 
+  };
+
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_PERSON':
+      return {...state,
+        contacts: [...state.contacts, action.data]}
+    default:
+      return state;
   }
-    
-  function handleSubmit(e) {
-    if(person !== '') {
-      props.handleSubmit(person);
-      setPerson('');
-    }
-    e.preventDefault();
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" 
-        placeholder="Add new contact" 
-        onChange={handleChange} 
-        value={person} />
-      <button type="submit">Add</button>
-    </form>
-  );
 }
 
-function PeopleList(props) {
-  const arr = props.data;
-  const listItems = arr.map((val, index) =>
-    <li key={index}>{val}</li>
-  );
-  return <ul>{listItems}</ul>;
-}
-
-function ContactManager(props) {
-  const [contacts, setContacts] = useState(props.data);
-
-  function addPerson(name) {
-    setContacts([...contacts, name]);
-  }
-
-  return (
-    <div>
-      <AddPersonForm handleSubmit={addPerson} />
-      <PeopleList data={contacts} />
-    </div>
-  );
-}
-const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
-
+const store = createStore(reducer);
 ReactDOM.render(
-  <ContactManager data={contacts} />, 
+  <Provider store={store}>
+    <AddPersonForm />
+    <PeopleList />
+  </Provider>,
   document.getElementById('root')
 );
